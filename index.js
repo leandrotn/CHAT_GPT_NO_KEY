c3c9ea91c5cc971cf5ce42e59933db79672d777c
leandrotn <74495390+leandrotn@users.noreply.github.com>
2024-01-14 16:05:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const axios = require('axios');
+
+const client = new Discord.Client({
+    intents: [32767]
+});
+const apiUrl = 'https://chatgpt.apinepdev.workers.dev/?question=';
+console.log('estou online!!')
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot || !message.mentions.has(client.user)) return;
+    const question = message.content.replace(/<@!\d+>/, '').trim();
+
+    try {
+        const response = await axios.get(apiUrl + encodeURIComponent(question));
+
+        message.reply(`${message.author}, ${response.data.answer}`);
+    } catch (error) {
+        console.error('Erro ao fazer a requisição à API:', error);
+        message.reply('Desculpe, ocorreu um erro ao processar sua pergunta.');
+    }
+});
+
+client.login('TOKEN_AQUI');
